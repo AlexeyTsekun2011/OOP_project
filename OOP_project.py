43bd01ad0877243822240614baae2fd4e6e60f79
Alexey <tsekunalexey@mail.ru>
2025-01-29 19:02:00 +1200

    Initial commit

--- OOP_project.py
+++ OOP_project.py
@@ -3,82 +3,76 @@ import time
 
 
 class Field:
-    def __init__(self,size,bunnies):
+    def __init__(self, size):
         self.size = size
         self.grid = []
         for i in range(size):
             self.grid.append([None] * self.size)
 
 
+
 class Bunny:
-    def __init__(self,size):
+    def __init__(self, size):
         placed = False
         while not placed:
-            self.coords =(random.randint(0,4)), random.randint(0,4)
-            self.coords[0] += (random.randint(-1, 1))
+            self.coords = (random.randint(0, 4)), random.randint(0, 4)
 
-            if self.coords != (0,0):
+            if self.coords != (0, 0):
                 placed = True
+        self.alive = True
 
 
 class Tiger:
     def __init__(self):
-        self.coords = [0,0]
+        self.coords = [0, 0]
         self.state = "Выследить добычу"
+        self.bunny = None
 
-    def countdown(self,name, countdown_time):
+    def countdown(self, countdown_time):
         while countdown_time > 0:
             print(f"До нового хода {countdown_time}")
             time.sleep(1)
             countdown_time -= 1
         else:
-            print("Время вышло")
+            print("Ход завершён")
 
+    def update_state(self, bunnies: list[Bunny]):
+        if self.state == "Выследить добычу":
+            self.seek_bunnies(bunnies)
+        elif self.state == "Атаковать добычу":
+            self.attack_bunnies(self.bunny)
+        elif self.state == "Бежать домой":
+            self.go_home()
 
-    def seek_bunnies(self,walk):
-        search = False
-        while not search:
-            self.coords[0] += (random.randint(-1, 1))
+    def seek_bunnies(self, bunnies: list[Bunny]):
+        self.coords[0] += (random.randint(-1, 1))
 
-            if self.coords[0] < 0:
-                self.coords[0] = 0
+        if self.coords[0] < 0:
+            self.coords[0] = 0
 
-            if self.coords[0] > 4:
-                self.coords[0] = 4
+        if self.coords[0] > 4:
+            self.coords[0] = 4
 
-            self.coords[1] += (random.randint(-1,1))
+        self.coords[1] += (random.randint(-1, 1))
 
-            if self.coords[1] < 0:
-                self.coords[1] = 0
+        if self.coords[1] < 0:
+            self.coords[1] = 0
 
-            if self.coords[1] > 4:
-                self.coords[1] = 4
+        if self.coords[1] > 4:
+            self.coords[1] = 4
+        for bunny in bunnies:
+            if abs(self.coords[0] - bunny.coords[0]) <= 0 and abs(self.coords[1] - bunny.coords[1]) <= 0:
+                self.state = "Атаковать добычу"
+                self.bunny = bunny
+                break
 
-
-
-    def attack_bunnies(self,attack):
-        find = False
-        while not find:
-            if self.coords[0] or self.coords[1]
+    def attack_bunnies(self, bunny):
+        if random.randint(0, 1):
+            self.bunny.alive = False
+            self.state = "Бежать домой"
+        else:
+            self.state = "Выследить добычу"
 
     def go_home(self):
-        go = False
-        while not go:
-            if #Заяц был пойман
-                self.coords[0] = 0
-                self.coords[1] = 0
-                #Спросить break или go = True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.coords[0] = 0
+        self.coords[1] = 0
